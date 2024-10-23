790bcec444ca3bc5858ca900417ae359fccc9284
Dan <danmakesthings@gmail.com>
2024-10-22 22:40:15 -0600

    Working through image import and imagemap bug.

--- src/utils/imageImports.js
+++ src/utils/imageImports.js
@@ -1,16 +1,8 @@
 // This file is auto-generated. Do not edit manually.
-// Generated 2024-10-23T03:07:24.910Z
+// Generated 2024-10-23T04:30:14.985Z
 
-import applethumbnail from '../images/apple-thumbnail.jpg';
-import ironnetthumbnail from '../images/ironnet-thumbnail.gif';
-import magicleapthumbnail from '../images/magic-leap-thumbnail.gif';
-import petalbrowclickthumbnail from '../images/petal-brow-click-thumbnail.gif';
-import petalmetricsthumbnail from '../images/petal-metrics-thumbnail.gif';
+import applethumbnail from 'src/images/apple-thumbnail.jpg';
 
 export const imageMap = {
-  '../images/apple-thumbnail.jpg': applethumbnail,
-  '../images/ironnet-thumbnail.gif': ironnetthumbnail,
-  '../images/magic-leap-thumbnail.gif': magicleapthumbnail,
-  '../images/petal-brow-click-thumbnail.gif': petalbrowclickthumbnail,
-  '../images/petal-metrics-thumbnail.gif': petalmetricsthumbnail
+  'src/images/apple-thumbnail.jpg': applethumbnail
 };
