ab5ac37091e055d167c8b38010284cd92cd805d3
ds1 <danmakesthings@gmail.com>
2025-08-10 23:55:00 -0600

    Updated case study details for agt mcp, thubble, ptz, site plan maker

--- src/utils/imageImports.js
+++ src/utils/imageImports.js
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
-// Generated 2025-08-11T04:32:22.991Z
+// Generated 2025-08-11T05:54:25.335Z
 
 import aicollabthumbnail from '../images/ai-collab-thumbnail.jpg';
 import applethumbnail from '../images/apple-thumbnail.jpg';
@@ -16,16 +16,20 @@ import picksithumbnail from '../images/picksi-thumbnail.jpg';
 import systemarchitecture from '../images/system-architecture.jpg';
 import agtregistryarchitecture from '../images/agt-registry-architecture.jpg';
 import agtregistrydiscovery from '../images/agt-registry-discovery.jpg';
-import thubbleworkflow from '../images/thubble-workflow.jpg';
-import thubblesynthesis from '../images/thubble-synthesis.jpg';
 import youtubettsinterface from '../images/youtube-tts-interface.jpg';
 import youtubettsworkflow from '../images/youtube-tts-workflow.jpg';
 import agtecosystem from '../images/agt-ecosystem.jpg';
 import agtfinancialmodel from '../images/agt-financial-model.jpg';
 import ptzinterfacedemo from '../images/ptz-interface-demo.jpg';
 import ptzcontrolmodes from '../images/ptz-control-modes.jpg';
-import siteplaninterface from '../images/siteplan-interface.jpg';
-import siteplancollaboration from '../images/siteplan-collaboration.jpg';
+import siteplanpdfalignment from '../images/siteplan-pdf-alignment.gif';
+import siteplancameraplacement from '../images/siteplan-camera-placement.gif';
+import siteplanzoneediting from '../images/siteplan-zone-editing.gif';
+import siteplanexportprocess from '../images/siteplan-export-process.gif';
+import siteplanworkflow from '../images/siteplan-workflow.jpg';
+import siteplanarchitecture from '../images/siteplan-architecture.jpg';
+import siteplaneditor from '../images/siteplan-editor.jpg';
+import siteplanexport from '../images/siteplan-export.jpg';
 import securitydashboardprocess from '../images/security-dashboard-process.jpg';
 import securitydashboardarchitecture from '../images/security-dashboard-architecture.jpg';
 import dsdevpreviewthumbnail from '../images/dsdev-preview-thumbnail.jpg';
@@ -50,16 +54,20 @@ export const imageMap = {
   '/images/system-architecture.jpg': systemarchitecture,
   '/images/agt-registry-architecture.jpg': agtregistryarchitecture,
   '/images/agt-registry-discovery.jpg': agtregistrydiscovery,
-  '/images/thubble-workflow.jpg': thubbleworkflow,
-  '/images/thubble-synthesis.jpg': thubblesynthesis,
   '/images/youtube-tts-interface.jpg': youtubettsinterface,
   '/images/youtube-tts-workflow.jpg': youtubettsworkflow,
   '/images/agt-ecosystem.jpg': agtecosystem,
   '/images/agt-financial-model.jpg': agtfinancialmodel,
   '/images/ptz-interface-demo.jpg': ptzinterfacedemo,
   '/images/ptz-control-modes.jpg': ptzcontrolmodes,
-  '/images/siteplan-interface.jpg': siteplaninterface,
-  '/images/siteplan-collaboration.jpg': siteplancollaboration,
+  '/images/siteplan-pdf-alignment.gif': siteplanpdfalignment,
+  '/images/siteplan-camera-placement.gif': siteplancameraplacement,
+  '/images/siteplan-zone-editing.gif': siteplanzoneediting,
+  '/images/siteplan-export-process.gif': siteplanexportprocess,
+  '/images/siteplan-workflow.jpg': siteplanworkflow,
+  '/images/siteplan-architecture.jpg': siteplanarchitecture,
+  '/images/siteplan-editor.jpg': siteplaneditor,
+  '/images/siteplan-export.jpg': siteplanexport,
   '/images/security-dashboard-process.jpg': securitydashboardprocess,
   '/images/security-dashboard-architecture.jpg': securitydashboardarchitecture,
   '/images/dsdev-preview-thumbnail.jpg': dsdevpreviewthumbnail,
