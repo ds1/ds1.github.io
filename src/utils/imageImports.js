95ad4387dadd4804de3ba707fa8aeb0f24683b5f
ds1 <danmakesthings@gmail.com>
2025-08-10 22:19:20 -0600

    Added case study draft content and placeholder images

--- src/utils/imageImports.js
+++ src/utils/imageImports.js
@@ -1,11 +1,35 @@
 // This file is auto-generated. Do not edit manually.
-// Generated 2025-07-29T05:58:21.575Z
+// Generated 2025-08-11T04:16:04.938Z
 
 import aicollabthumbnail from '../images/ai-collab-thumbnail.jpg';
 import applethumbnail from '../images/apple-thumbnail.jpg';
 import magicleapthumbnail from '../images/magic-leap-thumbnail.gif';
 import petalmetricsthumbnail from '../images/petal-metrics-thumbnail.gif';
+import agtregistrythumbnail from '../images/agt-registry-thumbnail.jpg';
+import thubblethumbnail from '../images/thubble-thumbnail.jpg';
+import youtubettsthumbnail from '../images/youtube-tts-thumbnail.jpg';
+import agtdomainsthumbnail from '../images/agt-domains-thumbnail.jpg';
+import ptzcontrollerthumbnail from '../images/ptz-controller-thumbnail.jpg';
+import siteplanthumbnail from '../images/siteplan-thumbnail.jpg';
+import securitydashboardthumbnail from '../images/security-dashboard-thumbnail.jpg';
+import dsdevpreviewthumbnail from '../images/dsdev-preview-thumbnail.jpg';
 import systemarchitecture from '../images/system-architecture.jpg';
+import agtregistryarchitecture from '../images/agt-registry-architecture.jpg';
+import agtregistrydiscovery from '../images/agt-registry-discovery.jpg';
+import thubbleworkflow from '../images/thubble-workflow.jpg';
+import thubblesynthesis from '../images/thubble-synthesis.jpg';
+import youtubettsinterface from '../images/youtube-tts-interface.jpg';
+import youtubettsworkflow from '../images/youtube-tts-workflow.jpg';
+import agtecosystem from '../images/agt-ecosystem.jpg';
+import agtfinancialmodel from '../images/agt-financial-model.jpg';
+import ptzinterfacedemo from '../images/ptz-interface-demo.jpg';
+import ptzcontrolmodes from '../images/ptz-control-modes.jpg';
+import siteplaninterface from '../images/siteplan-interface.jpg';
+import siteplancollaboration from '../images/siteplan-collaboration.jpg';
+import securitydashboardprocess from '../images/security-dashboard-process.jpg';
+import securitydashboardarchitecture from '../images/security-dashboard-architecture.jpg';
+import dsdevportalworkflow from '../images/dsdev-portal-workflow.jpg';
+import dsdevportalgrid from '../images/dsdev-portal-grid.jpg';
 import daneldorado from '../images/dan-eldorado.jpg';
 
 export const imageMap = {
@@ -13,6 +37,30 @@ export const imageMap = {
   '/images/apple-thumbnail.jpg': applethumbnail,
   '/images/magic-leap-thumbnail.gif': magicleapthumbnail,
   '/images/petal-metrics-thumbnail.gif': petalmetricsthumbnail,
+  '/images/agt-registry-thumbnail.jpg': agtregistrythumbnail,
+  '/images/thubble-thumbnail.jpg': thubblethumbnail,
+  '/images/youtube-tts-thumbnail.jpg': youtubettsthumbnail,
+  '/images/agt-domains-thumbnail.jpg': agtdomainsthumbnail,
+  '/images/ptz-controller-thumbnail.jpg': ptzcontrollerthumbnail,
+  '/images/siteplan-thumbnail.jpg': siteplanthumbnail,
+  '/images/security-dashboard-thumbnail.jpg': securitydashboardthumbnail,
+  '/images/dsdev-preview-thumbnail.jpg': dsdevpreviewthumbnail,
   '/images/system-architecture.jpg': systemarchitecture,
+  '/images/agt-registry-architecture.jpg': agtregistryarchitecture,
+  '/images/agt-registry-discovery.jpg': agtregistrydiscovery,
+  '/images/thubble-workflow.jpg': thubbleworkflow,
+  '/images/thubble-synthesis.jpg': thubblesynthesis,
+  '/images/youtube-tts-interface.jpg': youtubettsinterface,
+  '/images/youtube-tts-workflow.jpg': youtubettsworkflow,
+  '/images/agt-ecosystem.jpg': agtecosystem,
+  '/images/agt-financial-model.jpg': agtfinancialmodel,
+  '/images/ptz-interface-demo.jpg': ptzinterfacedemo,
+  '/images/ptz-control-modes.jpg': ptzcontrolmodes,
+  '/images/siteplan-interface.jpg': siteplaninterface,
+  '/images/siteplan-collaboration.jpg': siteplancollaboration,
+  '/images/security-dashboard-process.jpg': securitydashboardprocess,
+  '/images/security-dashboard-architecture.jpg': securitydashboardarchitecture,
+  '/images/dsdev-portal-workflow.jpg': dsdevportalworkflow,
+  '/images/dsdev-portal-grid.jpg': dsdevportalgrid,
   '/images/dan-eldorado.jpg': daneldorado
 };
